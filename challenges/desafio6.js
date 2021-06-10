daaedc67d69bf0d96d97547de8577c99c26a01fa
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-06-10 17:46:22 -0300

    requisitos 1 ao 10 feitos

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,4 @@
-db.produtos.updateMany({nome: {$in: ["Big Mac", "Quarteirão com Queijo"]}}, {$push: {ingredientes: "bacon"}});
-db.produtos.find({}, {nome: 1, ingredientes: 1, _id: 0});
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, { $push: { ingredientes: "bacon" } },
+);
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
