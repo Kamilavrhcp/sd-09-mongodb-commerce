f5f7633a8d4bb948c0388f221aa5a22a1a807cc1
jhon <jhonatan.arguello@gmail.com>
2021-06-10 16:40:03 -0300

    Finalizaçao do requisito 5.

---
+++ challenges/desafio5.js
@@ -0,0 +1,19 @@
+use("commerce");
+
+db.produtos.updateMany(
+  {
+    nome: { $ne: "McChicken" },
+  },
+  {
+    $addToSet: { ingredientes: "ketchup" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    ingredientes: true,
+  },
+);
