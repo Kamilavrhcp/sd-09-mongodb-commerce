7c9e97ec41cd0141071558aa3bf184ae77cad4b3
Pablo Fernandes <pabloprates@gmail.com>
2021-06-10 15:39:31 -0300

    Challenge Two - correction again

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,2 +1,3 @@
-db.produtos.updateMany({ $valorUnitario: { exists: false } }, { $set: { valorUnitario: 0 } });
+db.produtos.updateMany({
+  $valorUnitario: { exists: false } }, { $set: { valorUnitario: numberDecimal(0.00) } });
 db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({
-  valorUnitario: { $exists: false } }, { $set: { valorUnitario: NumberDecimal(0) } });
+  valorUnitario: { $exists: false } }, { $set: { avaliacao: NumberInt(0) } });
 db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
