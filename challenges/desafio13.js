ed0051d2eaaa0041326576855e4528fda11070ed
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-10 16:58:57 -0300

    req 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,6 @@
 db.produtos.updateMany(
-  { "valoresNutricionais.1.percentual": { $gte: 40 } },
+  { valoresNutricionais: { $elemMatch: { 
+    $and: [{ tipo: "sódio" }, { percentual: { $gte: 40 } }] } } },
  { $push: { tags: "muito sódio" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
