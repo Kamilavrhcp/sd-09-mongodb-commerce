25f3fad8f393180a96e596dc9bd80553c2cefb61
Diogenes <diogenesfelipe.s@outlook.com>
2021-06-10 16:20:59 -0300

    fix/Desafio5 and feat/Desafio6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -4,7 +4,7 @@ db.produtos.updateMany(
     ingredientes: { $elemMatch: { $ne: "ketchup" } },
   },
   {
-    $push: { ingredientes: { $each: ["ketchup"] } },
+    $push: { ingredientes: "ketchup" },
   },
 );
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
