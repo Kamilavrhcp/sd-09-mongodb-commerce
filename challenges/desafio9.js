f4eb712fdc1de4f21b6af9b7e360bf9fd93150d1
LucioOSilva <lucioosilva00@gmail.com>
2021-06-10 16:10:41 -0300

    added req9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+// use('commerce');
+db.produtos.updateMany(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
