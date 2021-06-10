b49701089600266d8778059d52a150347f845ea0
tiago-yoneda <xyonedax@gmail.com>
2021-06-10 14:25:15 -0300

    desafio 1 - completed

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,13 @@
-// Bora começar
+// 1 - Inclua o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
+// Para isso, escreva no arquivo desafio1.js duas queries, nesta ordem:
+use("commerce");
+// Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
+db.produtos.updateMany(
+  {},
+  { $set: { criadoPor: "Ronald McDonald" } }
+);
+// Crie uma query que retorne o nome e criadoPor de todos os produtos.
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, criadoPor: 1 }
+);
