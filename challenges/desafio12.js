91f68874e1547c1b13525b866d3691330c8c1d9e
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-06-10 16:20:56 -0300

    Fix requisito 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -3,14 +3,13 @@ db.produtos.updateMany(
   { 
     $push: 
     { 
-      valoresNutricionais: { 
-        $each: ["combo", "tasty"], $sort: { percentual: -1 },
-      },
+      valoresNutricionais:
+      {
+        $each: [],
+        $sort: { percentual: -1 },
+      }, 
     },
   },
 );
 
-db.produtos.find(
-  {}, 
-  { _id: 0, nome: 1, valoresNutricionais: 1 },
-);
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
