a38122b199c6132ebf77dd8dfd5cff4ea1f09491
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-10 15:42:08 -0300

    req 4,6,7

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
 db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificao: true } });
-db.produtos.find({ ultimaModificao: { $exists: true } });
+db.produtos.find({ ultimaModificao: { $exists: true } }, { _id: 0, nome: 1 });
