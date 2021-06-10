f295548495713e20b2f82aa7fc380b188214e141
Diogenes <diogenesfelipe.s@outlook.com>
2021-06-10 19:28:33 -0300

    fix/Desafio17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -2,4 +2,4 @@ db.produtos.createIndex(
   { descricao: "text" },
   { default_language: "portuguese" },
 );
-db.produtos.find({ $text: { $search: "frango hamburguer" } });
+db.produtos.countDocuments({ $text: { $search: "frango hamburguer" } });
