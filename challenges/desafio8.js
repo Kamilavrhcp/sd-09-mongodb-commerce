ed7eb26a9b2cca30a6f7d3b59266efa06b3b6ddd
LucioOSilva <lucioosilva00@gmail.com>
2021-06-10 16:08:51 -0300

    added req8

---
+++ challenges/desafio8.js
@@ -0,0 +1,10 @@
+// use('commerce');
+db.produtos.updateMany(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
