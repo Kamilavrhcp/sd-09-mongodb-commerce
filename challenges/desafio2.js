74f95898294ae2b837e32f42a157d90652900558
Christopher <chris.tryber@gmail.com>
2021-06-10 15:06:18 -0300

    fix desafio2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,4 +2,4 @@ db.produtos.updateMany(
     { valorUnitario: { $exists: 0 } },
     { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
-db.produtos.find({ valorUnitario: { $exists: 1 } });
+db.produtos.find({ valorUnitario: { $exists: 1 } }, { nome: 1, valorUnitario: 1, _id: 0 });
