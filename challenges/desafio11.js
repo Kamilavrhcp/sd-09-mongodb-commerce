a1c8a2c79b16fdff8572b5a5c5a37e6848f95801
LucioOSilva <lucioosilva00@gmail.com>
2021-06-10 17:24:21 -0300

    added req13

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,13 +1,7 @@
 // use('commerce');
 db.produtos.updateMany(
   {},
-  { $push: {
-      tags: {
-        $each: ["combo", "tasty"],
-        $sort: 1,
-      },
-    },
-  },
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } },
 );
 
 db.produtos.find(
