bbf2fd792cd32dc89f4338b455893fa868853f82
Pablo Fernandes <pabloprates@gmail.com>
2021-06-10 16:15:42 -0300

    Challenge Five - Refactored

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,8 +1,7 @@
 db.produtos.updateMany({
   nome: { $ne: "McChicken" },
-  ingredientes: { $ne: "ketchup" },
   },
   {
-    $push: { ingredientes: "ketchup" },
+    $addToSet: { ingredientes: "ketchup" },
   });
 db.produtos.find({ }, { nome: 1, ingredientes: 1, _id: 0 });
