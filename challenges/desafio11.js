c80d104308735db0e3cb953454e2a0beb5ebccec
edison-ps <edison@osite.com.br>
2021-06-10 18:20:16 -0300

    Desafio 11 V1.01

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"],
-  $sort: { tags: -1 } } } });
+  $sort: 1 } } });
 db.produtos.find({}, { _id: false, nome: true, tags: true });
