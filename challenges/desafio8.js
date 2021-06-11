ee1261abec728a79080dd8ae58b3e667dc02f2af
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-11 11:37:59 -0300

    Desafio: 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,4 +1,4 @@
 // use('commerce');
-db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: 1 } });
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
 // use('commerce');
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
