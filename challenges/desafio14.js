58ec0aa5dce81a517db5b5e4880e0ae9d15cf7f9
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-11 20:43:09 -0300

    Desafio: 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -2,7 +2,8 @@
 db.produtos.updateMany({
   valoresNutricionais: {
     $elemMatch: {
-      tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } }, {
-        $push: { tags: "contém sódio" } });
+      tipo: "sódio", percentual: { $gt: 20, $lt: 40 },
+    } } },
+    { $push: { tags: "contém sódio" } });
 // use('commerce');
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
