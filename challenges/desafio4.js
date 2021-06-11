31fcf71059133d3108388fa7ef55e742f7e07691
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-10 21:10:17 -0300

    Desafio: 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,3 @@
 db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
 db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: false, nome: true });
+// coloquei false para que futuramente me lembre que também pode usar tanto 0, 1 para representar dados booleanos
