8ca9d7174a475f3e5ec88f7cbe36a02edf6eaa2f
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-11 13:50:13 -0300

    Desafio: 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,6 +3,6 @@ db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 // use('commerce');
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 // use('commerce');
-db.produtos.updateOne({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.7": 120 } });
+db.produtos.updateOne({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 // use('commerce');
 db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
