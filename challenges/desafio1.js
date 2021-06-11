bea46debf7ed3fb20a6be0759188e1997c16f32b
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-11 12:26:49 -0300

    Requisitos 1 e 2 - correções_3 :[

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { $set: { criadoPor: "Ronald MacDolnald" } });
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDolnald" } });
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
