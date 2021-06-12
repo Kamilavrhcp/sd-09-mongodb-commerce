849780e162f0feb1e0f9f3de4a22408fc0692802
Christopher <chris.tryber@gmail.com>
2021-06-12 17:17:14 -0300

    Requisitos 14, 15 e 16

---
+++ challenges/desafio14.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $lt: 40, $gt: 20 } } } }, 
+  { $push: { tags: "contém sódio" } },
+);
+db.produtos.find({}, { tags: 1, nome: 1, _id: 0 });
