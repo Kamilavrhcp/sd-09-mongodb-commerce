c8e26cc3d73ca8221fcac95ffad0f0754baeb5fe
Christopher <chris.tryber@gmail.com>
2021-06-12 17:33:19 -0300

    Requisitos 17 e 18

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,2 @@
-db.produtos.find({ $text: { $search: "frango hamburguer" } });
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+db.produtos.countDocuments({ $text: { $search: "frango hamburguer" } });
