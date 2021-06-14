7f2a5049a2219c622b09902fa9b8d7be3bbc06e5
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-06-14 16:57:44 -0300

    Requirement 20 code correction again

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,3 +1,3 @@
-db.updateMany({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
 
 db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
