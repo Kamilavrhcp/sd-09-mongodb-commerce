6419e065683554d143d945b1038b5722d52db224
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-06-14 02:32:20 -0300

    desafio 21

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -2,4 +2,4 @@ db.produtos.updateMany(
   { nome: "Big Mac" },
   { $unset: { curtidas: "" } },
 );
-db.produtos.find({}, { nome: 1, curtidas: 1, _id: 0, });
+db.produtos.find({}, { nome: 1, curtidas: 1, _id: 0 });
