e7bcdfbc930cbd5db50d565e74096fad490376a6
pedrohrbispo <pedroenrique.400@gmail.com>
2021-06-15 16:35:38 -0300

    faz ate o requisito 19 e conserta o 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,7 +3,7 @@ db.produtos.updateMany(
     valorUnitario: { $exists: false },
   },
   { 
-    $set: { valorUnitario: NumberDecimal("0,00") },
+    $set: { valorUnitario: NumberDecimal("0.00") },
   },
   { 
     upsert: true, 
