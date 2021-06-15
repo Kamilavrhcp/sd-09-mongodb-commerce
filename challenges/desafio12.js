ea787dec7747fe3ee4839b0f505e816beed82fb4
Cainan <cainan6697@gmail.com>
2021-06-15 10:20:04 -0300

    Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push:
+    {
+      valoresNutricionais: {
+        $each: ["combo", "tasty"], $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
