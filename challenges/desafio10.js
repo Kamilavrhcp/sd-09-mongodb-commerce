84470e81d87a4d8a6dbb32abf5d128878c5e7a20
Cainan <cainan6697@gmail.com>
2021-06-14 21:54:25 -0300

    Fix Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,15 +1,16 @@
 db.produtos.updateMany(
-  { $set: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
 );
 
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $push: { $each: [{ $inc: { vendasPorDia: 60 } }] }, $position: 4 },
+  { $inc: { "vendasPorDia.3": 60 } },
 );
 
 db.produtos.updateOne(
-  { $all: { tags: { $in: ["bovino", "pão"] } } },
-  { $push: { $each: [{ $inc: { vendasPorDia: 120 } }] }, $position: 7 },
+  { tags: { $all: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
