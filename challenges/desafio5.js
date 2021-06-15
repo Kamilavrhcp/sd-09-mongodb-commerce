0af3f70f4660c4e0a9b7901e3983821d44b110ac
fomes <contato.felipeluna@gmail.com>
2021-06-15 18:17:55 -0300

    challenge 10, 5 error

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,1 +1,6 @@
-db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $push: { ingredientes: "ketchup" } });
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } }, 
+  { $push: { ingredientes: "ketchup" } }, 
+  { upsert: true },
+);
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
