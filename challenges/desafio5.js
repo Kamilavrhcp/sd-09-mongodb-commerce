b2f2abbb9cc432e2ca9377646e36ea2ebfb0d323
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-06-16 17:28:40 -0300

    Task 05 - FIX LINT

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-  { nomes: { $nin: ["McChicken"] }},
+  { nomes: { $nin: ["McChicken"] } },
   { $addToSet: { ingrediente: "ketchup" } },
 );
 
-db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 })
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
