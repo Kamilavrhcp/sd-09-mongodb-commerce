9229a8a665f9fcdfcb80a4f58adfd75a9bc3f35b
Arthur Nunes <coletivosuburbano@gmail.com>
2021-06-16 18:22:33 -0300

    ex 6 ao 22 ESlint

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: true,
+      },
+    },
+  },
+);
+
+db.produtos.find({}, { _id: false, nome: true, tags: true });
