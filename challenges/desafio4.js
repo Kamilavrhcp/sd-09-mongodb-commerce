1a78c7d4bc2955fd2a2963abe6af2e6e0ff50771
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-06-16 17:20:55 -0300

    Task 04 - FIX

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaMoficacao: true } },
+  { $currentDate: { ultimaModificacao: true } },
 );
 
 db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
