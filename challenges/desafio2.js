76a849aa474fa1652c1a9fcc03088a4b23a62988
Antonio Schappo <antonioschappo@gmail.com>
2021-06-20 11:24:08 -0300

    third try challenge 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({ valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: 0.00, "valorUnitario.tipo": "NumberDecimal" } });
+  { $set: { valorUnitario: 0.00, tipo: "NumberDecimal" } });
 
 db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
