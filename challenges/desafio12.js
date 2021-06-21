c9eb6ab541ed5bbd2ae3fcc22b4c823ae1e72164
Kevin Fraga <kevinciolac@gmail.com>
2021-06-21 00:01:15 -0300

    desafio :one::two: fix

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,4 +2,4 @@ db.produtos.updateMany(
   {},
   { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
 );
-db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
