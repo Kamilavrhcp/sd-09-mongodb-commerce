555af63bb8f47e273a0ce6900070888c16105210
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-27 14:45:38 -0300

    Challenge six

---
+++ challenges/desafio6.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  {
+    $addToSet: {
+      ingredientes: "bacon",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
