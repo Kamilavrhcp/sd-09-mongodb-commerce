ea44e592313dd776a6a1c14d2ce16118adbae565
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-27 14:39:03 -0300

    Challenge five

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,19 +1,17 @@
 db.produtos.updateMany(
   {
     nome: {
-      $ne: "McChicken",
-    },
-    ingredientes: {
-      $ne: "ketchup",
+      $not: /McChicken/,
     },
   },
   {
-    $push: {
+    $addToSet: {
       ingredientes: "ketchup",
     },
   },
 );
 
 db.produtos.find(
+  {},
   { _id: 0, nome: 1, ingredientes: 1 },
 );
