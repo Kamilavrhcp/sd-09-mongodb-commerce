eaa46b8b01b603a5b2a7ebeef2e65a3c3dc8f652
Micaela Alves <micaela.alves93@gmail.com>
2021-07-02 23:18:45 -0300

    Req 6 fix

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,7 @@
 db.produtos.updateMany(
     { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
-    { $push: { ingredientes: "Bacon" } },
+    { $push: { ingredientes: "bacon" } },
+    { upsert: true },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
