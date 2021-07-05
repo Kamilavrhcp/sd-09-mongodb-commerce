a59940a1dac25dc12fae508756630ad3f00e1ccf
Uibirá <usbarreto@gmail.com>
2021-07-05 18:13:42 -0300

    D05 addToSet

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
-  { $set: { ingredientes: "ketchup" } },
+  { $addToSet: { ingredientes: "ketchup" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
