f99e710e96de3f865368bcbc7469b9098403ab79
Víctor Cançado <victor.cancado@gmail.com>
2021-07-06 22:19:47 -0300

    Adiciona requisitos 5 e 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({ 
+$and: [
+{ nome: { $ne: "McChicken" } },
+{ ingredientes: { $nin: ["ketchup"] } },
+] }, 
+{ 
+$push: { ingredientes: "ketchup" }, 
+});
+db.produtos.find({ }, { nome: 1, ingredientes: 1, _id: 0 });
