b651aa325f92ba2ae63d7bc0ff2f20c2f12c409c
Alstent <rafaelasltent@gmail.com>
2021-07-07 14:26:59 +0200

    resolvendo lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -4,4 +4,4 @@ db.produtos.updateMany({ tags: "bovino" }, { $set: { avaliacao: NumberInt("5") }
 
 db.produtos.updateMany({ tags: "ave" }, { $set: { avaliacao: NumberInt("3") } });
 
-db.produtos.find({}, {  _id: 0, nome: 1, avaliacao: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
