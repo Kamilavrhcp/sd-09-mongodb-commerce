14f884132469a460769140d2669c25e9ed1e674e
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-07-07 19:58:18 -0300

    Requisitos de 10 a 18

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,10 +1,10 @@
 db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $push: { vendasPorDia: { $each: [60], $position: 3 } } },
+  { $inc: { "vendasPorDia.3": 60 } },
 );
 db.produtos.updateOne(
   { tags: { $in: ["bovino", "pão"] } },
-  { $push: { vendasPorDia: { $each: [120], $position: 6 } } },
+  { $inc: { "vendasPorDia.6": 120 } },
 );
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
