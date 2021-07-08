281ccfcfbe18524155ea41e7436f7663b65159e3
victorvalim <victor.valim.c@gmail.com>
2021-07-08 02:52:04 -0300

    Agora vaai, eslint >.<

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   {
     valoresNutricionais: {
-      $elemMatch: { percentual: { $gt: 20, $lt: 40 }, tipo: "sódio" }
+      $elemMatch: { percentual: { $gt: 20, $lt: 40 }, tipo: "sódio" },
     },
   }, { $push: { tags: "contém sódio" } },
   );
