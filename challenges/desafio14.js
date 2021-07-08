d3950d64bee7ae68d2045f75655221f85ce0ac26
victorvalim <victor.valim.c@gmail.com>
2021-07-08 02:43:42 -0300

    Eslint

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,12 @@
 db.produtos.updateMany({
-  valoresNutricionais:
-    { $elemMatch: { percentual: { $gt: 20, $lt: 40 }, tipo: "sódio" } }
+  valoresNutricionais: {
+    $elemMatch: {
+      percentual: {
+        $gt: 20,
+        $lt: 40
+      }, tipo: "sódio"
+    }
+  }
 },
   { $push: { tags: "contém sódio" } });
 
