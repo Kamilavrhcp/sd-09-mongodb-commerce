9be1371bc95497a28e54ba6004e54ff4ea5f8b77
Alstent <rafaelasltent@gmail.com>
2021-07-08 14:52:23 +0200

    desafio 5, 6, 7, 8, 9 e 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
+// db.produtos.updateMany( {}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } }); | diferenca? nao faco a minima ideia...
+
+db.produtos.updateMany({ nome: "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
+// '.updateOne()' talvez seria melhor aki, mas como o collection é pequena, noa importa muito
+
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $set: { "vendasPorDia.6": 120 } });
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
