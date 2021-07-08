4feabe009539b3335e6dbae0a00273bb328564ee
victorvalim <victor.valim.c@gmail.com>
2021-07-08 02:46:09 -0300

    Eslinttttt

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,4 +1,6 @@
-db.produtos.updateMany({ valoresNutricionais: {$elemMatch :{ percentual:{$gt:40}, tipo:"sódio"}}},
-{ $push: { tags: "muito sódio" } });
+db.produtos.updateMany(
+  { valoresNutricionais: {$elemMatch :{ percentual:{$gt:40}, tipo:"sódio"}
+},
+}, { $push: { tags: "muito sódio" } });
 
 db.produtos.find({}, { nome: 1, tags:1, _id: 0 });
