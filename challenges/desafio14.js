1a669d67388d83f7801ce89f704ff039d3bb5075
Víctor Cançado <victor.cancado@gmail.com>
2021-07-07 21:42:50 -0300

    Adiciona requisitos 12, 13 e 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+{ valoresNutricionais: { 
+$all: [
+{ $elemMatch: 
+{ tipo: "sódio", percentual: { $gt: 20 } } },
+{ $elemMatch: { tipo: "sódio", percentual: { $lt: 40 } } },
+],
+} },
+{ $push: { tags: "contém sódio" } },
+);
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 });
