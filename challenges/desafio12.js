e2faf17207c429b41a369cdd161c4b4f2fbf2eac
victorvalim <victor.valim.c@gmail.com>
2021-07-08 03:00:41 -0300

    Erro bobo === hora de dormir

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({},
-  { $push: { "valoresNutricionais.percentual": { $each: [], $sort: -1 } } });
+  { $push: { "valoresNutricionais.percentual": { $each: [], $sort: { percentual: -1 } } } });
 
 db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
