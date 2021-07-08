a1e219a7c3dcac1b00368bf521b54de3e55a1c24
Rodrigo <deltadigo@gmail.com>
2021-07-08 09:34:53 -0300

    Requisito 4 - Correção de typo

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -3,7 +3,7 @@ db.produtos.updateOne(
   { $currentDate: { ultimaModificacao: { $type: "date" } } },
 );
 
-db.produtos.fin(
+db.produtos.find(
   { ultimaModificacao: { $exists: true } },
   { nome: 1, _id: 0 },
 );
