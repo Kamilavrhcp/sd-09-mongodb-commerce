9a618ff1d7ba7ffb1185c5b36b27cb9459fead7f
victorvalim <victor.valim.c@gmail.com>
2021-07-08 01:55:09 -0300

    Eslint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,5 +4,4 @@ db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
 db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 
-
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
